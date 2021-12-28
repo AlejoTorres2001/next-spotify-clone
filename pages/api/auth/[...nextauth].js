4044d9145f5460bcb5649e4125cf387e2620f948
T0w3r <torres.alejo2001@gmail.com>
2021-12-28 13:18:39 -0300

    jwt callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,30 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { refreshAccessToken } from "spotify-web-api-node/src/server-methods"
+import spotifyApi ,{ LOGIN_URL } from "../../../lib/spotify"
+
+const refreshAccessToken = async (token) => {
+    try {
+      spotifyApi.setAccessToken(token.accessToken);
+      spotifyApi.setRefreshToken(token.refreshToken);
+      const {body:refreshedToken} = await spotifyApi.refreshAccessToken();
+      console.log("refreshed token is",refreshedToken)
+      return{
+        ...token,
+        accessToken: refreshedToken.access_token,
+        accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000, //= 1hour as 3600 seconds returns from spotify api
+        refreshToken: refreshedToken.refresh_token ?? token.refreshToken,// replace if new one came back else fall back to old refresh token
+      }
+    } catch (error) {
+      console.error(error)
+      return {
+        ...token,
+        error:"RefreshAccessTokenError"
+      }
+    }
+      
+  }
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,9 +32,34 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      authorization:LOGIN_URL
+      authorization:LOGIN_URL,
       
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages:{signIn:'/login'},
+  callbacks:{
+    async jwt({token,account,user}){
+      //initial sign in
+      if(account && user){
+        return{
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username:account.providerAccountId,
+          accessTokenExpires:account.expires_at * 1000, //convert to ms
+        }
+      }
+      //return previus token if it does not expired yet
+      if (Date.now() < token.accessTokenExpires){
+        console.log('token not expired yet')
+        return token
+      }
+      //token expired, refresh it
+      console.log('token expired, refresh it')
+      return await refreshAccessToken(token)
+    }
+
+  }
 })
