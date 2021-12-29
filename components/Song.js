8eee5edc762179e632b81db64a12da6d1ec7b489
T0w3r <torres.alejo2001@gmail.com>
2021-12-29 12:51:30 -0300

    play song function

--- components/Song.js
+++ components/Song.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState,isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
 import  millisToMinutesAndSeconds  from "../lib/time";
 const Song = ({ order, track }) => {
   const SpotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying,setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = async () => {
+
+    setCurrentTrackId(track.track.id);
+    setIsPlaying(true);
+    await SpotifyApi.play({
+
+      uris:[track.track.uri],
+
+    });
+    
+  }
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer" onClick={playSong}>
       <div className="flex items-center space-x-4">
         <p>{order}</p>
         <img
