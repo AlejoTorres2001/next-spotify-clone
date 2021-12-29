c0211f486b6d7d4fcf580e3598acd9ad36c56a77
T0w3r <torres.alejo2001@gmail.com>
2021-12-29 14:54:46 -0300

    fixed 'illegal scopes error'

--- components/Player.js
+++ components/Player.js
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
-import { currentTrackIdState,isPlayingState } from "../atoms/songAtom";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
 
@@ -11,14 +11,37 @@ const Player = () => {
   const [currentTrackId, setCurrentTrakId] =
     useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
-  const [volume,setVolume] = useState(50);
+  const [volume, setVolume] = useState(50);
   const songInfo = useSongInfo();
-  console.log("song info", songInfo);
+
+  const fetchCurrentSong = async () => {
+    if (!songInfo) {
+      SpotifyApi.getMyCurrentPlayingTrack().then((res) => {
+        console.log("Now pLaying", res.body?.item?.id);
+        setCurrentTrakId(res.body?.item?.id);
+        SpotifyApi.getMyCurrentPlaybackState().then((res) => {
+          setIsPlaying(res.body?.is_playing);
+        });
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (SpotifyApi.getAccessToken() && !currentTrackId) {
+        fetchCurrentSong();
+        setVolume(50);
+    }
+  }, [currentTrackId, SpotifyApi, session]);
+
   return (
     <div>
       {/* left */}
       <div>
-        <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+        <img
+          className="hidden md:inline h-10 w-10 p-2"
+          src={songInfo?.album.images?.[0]?.url}
+          alt=""
+        />
       </div>
     </div>
   );
