c9bb43e5eaa204adeb217dd381a92f79f546b787
T0w3r <torres.alejo2001@gmail.com>
2021-12-29 17:01:44 -0300

    .

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -8,17 +8,17 @@ export async function middleware(req, res, next) {
     //1) have token
     //2) is an auth req
 
-    //if user have token and trys to log in 
-    if (token && pathname === "/login"){
-        return  NextResponse.redirect('/')
-    }
+    // //if user have token and trys to log in 
+    // if (token && pathname === "/login"){
+    //     return  NextResponse.redirect('/')
+    // }
     
     if (pathname.includes("/api/auth") || token ) {
         return NextResponse.next()
     }
     
-    // //REDIRECT TO LOGIN
-    // if (!token && pathname !== '/login'){
-    //     return NextResponse.redirect('/login')
-    // }
+    //REDIRECT TO LOGIN
+    if (!token && pathname !== '/login'){
+        return NextResponse.redirect('/login')
+    }
 }
